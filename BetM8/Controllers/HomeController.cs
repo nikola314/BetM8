21651e7ad40de4ac916ab8635a30470247351a21
Nikola Kesic <nikola.kesic97@gmail.com>
2019-05-07 02:04:07 +0200

    Add project files.

---
+++ BetM8/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BetM8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BetM8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public db_betm8Entities db = new db_betm8Entities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View(db.users.ToList());
+        }
+    }
+}
---
+++ BetM8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BetM8.MvcApplication" Language="C#" %>
